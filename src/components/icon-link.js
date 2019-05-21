e34cd54a343a879ead907dfadf0d74e1ed497b50
Mark Harwood <msharwood12@gmail.com>
2019-05-21 11:40:26 -0500

    Updating packages, adding noopener/noreferrer to iconlink, fixing String => string in proptype declaration

--- src/components/icon-link.js
+++ src/components/icon-link.js
@@ -1,16 +1,16 @@
-import React, { Children } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import './icon-link.scss'
 
 const IconLink = ({link, title, children}) => (
-  <a className="icon-link" href={link} title={title} target="_blank">
+  <a className="icon-link" href={link} title={title} target="_blank" rel="noopener noreferrer">
     {children}
   </a>
 )
 
 IconLink.propTypes = {
-    link: PropTypes.String,
+    link: PropTypes.string,
 }
 
 IconLink.defaultProps = {
